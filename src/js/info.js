8b79d451090c0fddbfb04fc423ab708aae8ade06
JoshuaThai <145885143+JoshuaThai@users.noreply.github.com>
2025-08-28 17:40:16 -0500

    Working on info.html
    Worked on second page in html. Added buttons that tells user if they can proceed further with survey.

---
+++ src/js/info.js
@@ -0,0 +1,9 @@
+function showHidden(){
+    document.getElementById("message").hidden = true;
+    document.getElementById("hidden-answers").hidden = false;
+}
+
+function showMessage(){
+    document.getElementById("message").hidden = false;
+    document.getElementById("hidden-answers").hidden = true;
+}
