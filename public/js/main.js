a01947a33152cc327f5bcbc1bbecd4687afba45f
JoshuaThai <145885143+JoshuaThai@users.noreply.github.com>
2025-09-04 17:37:38 -0500

    Routing Some Urls
    Routing the urls of the first two webpages and the unsuccessful webpage.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,11 @@
 function showNext(){
-    document.getElementById("nextButton").style.display = "flex"
+    document.getElementById("nextButton").style.display = "flex";
+    document.getElementById("exitButton").style.display = "none";
+}
+
+function exit(){
+    document.getElementById("exitButton").style.display = "flex";
+    document.getElementById("nextButton").style.display = "none";
 }
 
 function nextPage(link){
--- server.js
+++ server.js
@@ -7,11 +7,22 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended : true}));
 
 app.set('view engine', 'ejs');
-app.set("views", "./views");
+// app.set("views", "./views");
 
 app.get('/', (req, res) =>{
     console.log("Yes");
     res.render("index");
+})
+
+app.post('/', (req, res) =>{
+    // req will contain infromation being sent in
+    // res will contain information being sent out by server
+    let selected = req.body.submitButton;
+    if (selected == "Exit"){
+        res.redirect("/unsuccessful.html"); // redirect user to unsuccessful page.
+    } else{
+        res.render("info");
+    }
 })
 
 app.listen(3000);
