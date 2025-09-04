4f2737167c9098c1a3829e950526d58ee96f9792
JoshuaThai <145885143+JoshuaThai@users.noreply.github.com>
2025-09-04 16:29:33 -0500

    Restructured Project
    Restructured project to resemble typical project structure for the express stack

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 // Set up server.js
+
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({extended : true}));
+
+app.set('view engine', 'ejs');
+
+app.listen(3000);
