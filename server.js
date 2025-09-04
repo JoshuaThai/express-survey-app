3454a7ac33a94298186fd051851933c8571fd441
JoshuaThai <145885143+JoshuaThai@users.noreply.github.com>
2025-09-04 16:49:33 -0500

    Installed ejs into project

--- server.js
+++ server.js
@@ -3,8 +3,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"));
 app.use(express.urlencoded({extended : true}));
 
 app.set('view engine', 'ejs');
+app.set("views", "./views");
+
+app.get('/', (req, res) =>{
+    console.log("Yes");
+    res.render("index");
+})
 
 app.listen(3000);
