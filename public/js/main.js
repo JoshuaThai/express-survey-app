20e943148a216c4e5e40a6d68cc076ff380acf9b
JoshuaThai <joshua.thaisananikone@gmail.com>
2025-09-25 13:06:50 -0500

    Attempting to modify previous button
    Attempting to correct previous button convention and enable data to load on server side on survey when user reloads or return to the previous page.

--- public/js/main.js
+++ public/js/main.js
@@ -9,10 +9,30 @@ function exit(){
     document.getElementById("nextPage").value = "Exit";
 }
 
+function loadData(myData){
+    console.log("LOADING DATA");
+    for(const key in myData){
+        // let element = document.getElementById(key);
+        const input = document.querySelector(`[name="${key}"]`);
+        if(!input) continue;
+        if (input && input.type == 'radio'){
+             let element = document.querySelector(`input[name="${key}"][value="${myData[key]}"]`);
+             if(element) element.checked = true;
+        } else if(input && input.type == 'text'){
+            input.value = myData[key];
+        } else if(input && input.type == 'date'){
+
+        }
+    }
+}
+
 function nextPage(link){
     window.location.href = link;
 }
 
+module.exports = {
+        loadData
+};
 // form.addEventListener('submit', function(e) {
 //   e.preventDefault(); // This disables native validation unless you manually check
 //   // You must validate inputs manually here
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Set up server.js
-
 const express = require('express');
 const app = express();
+
+const myData = {}; // where data for form will be stored.
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended : true}));
@@ -11,12 +12,24 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
     console.log("Yes");
-    res.render("index");
-})
+    // load data into form.
+    // mainModule.loadData(myData);
+    // res.render("index");
+    res.render("index", {myData});
+});
 
 app.post('/', (req, res) =>{ //look at request coming from your web server
     // req will contain infromation being sent in
     // res will contain information being sent out by server
+    const formData = req.body; // Access form data from req.body
+    console.log('Received form data:', formData);
+    
+    // save data in pseudo server-side data storage.
+    for(const key in formData){
+        if (key == "nextPage" || key == "submitButton"){continue}
+        // console.log(key,"-", formData[key]);
+        myData[key] = formData[key];
+    }
     let selected = req.body.nextPage;
     let indexSelected = req.body.submitButton; // if user failed survey right away
     if (indexSelected == "Exit"){
@@ -28,9 +41,14 @@ app.post('/', (req, res) =>{ //look at request coming from your web server
     if (selected == "Exit"){
         res.redirect("/unsuccessful.html"); // redirect user to unsuccessful page.
     } else{
-        res.render(selected);
+        res.render(selected, {myData});
     }
-})
+});
+
+app.post('/info', (req, res) =>{
+    console.log("INFO PAGE LOADING");
+    return res.render('info', {myData});
+});
 
 
 
