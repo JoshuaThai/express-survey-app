ec24a03f0aed48d17ee57eb64b8f4b823c5015a4
JoshuaThai <joshua.thaisananikone@gmail.com>
2025-09-24 22:33:07 -0500

    Update info.js
    Enabled for survey to redirect users to ineligibility page if they are not from US.

--- public/js/info.js
+++ public/js/info.js
@@ -11,5 +11,10 @@ function hideInfo(){
     document.getElementById("hidden-answers").hidden = true;
     // Select by name and value
     // This line can help us.
-    // document.querySelector('input[name="yourRadioName"][value="yourRadioValue"]').checked = true;
+    // document.querySelector('input[name="yourRadioName"][value="yourRadioValue"]').checked = true
+    document.querySelector('input[name="zipCode"]').value = "00000";
+    document.querySelector('input[name="birth-date"]').value = "2001-01-01";
+    document.querySelector('input[name="gender"][value="Other"]').checked = true;
+    document.querySelector('input[name="hispanic"][value="No"]').checked = true;
+    document.querySelector('input[name="race"][value="White"]').checked = true;
 }
